e47451337d9bff19f05ef110a54f75fba3afbd1f
Alex35regi <128421218+Alex35regi@users.noreply.github.com>
2025-11-10 21:12:51 +0300

    My-DZ

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<clocale>
+#include<iostream>
+#include <Windows.h>
+
+int main()
+{  
+	setlocale(LC_ALL, "RU");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	std::cout << "Александр\n";
+	std::cout << 32 << "\n";
+
+	return EXIT_SUCCESS;
+}
